de17bfb76c77422dfc378f692a817ae3d6a172f9
Marco Antonio <marco.perez.cetis76@gmail.com>
2020-10-24 14:28:21 -0500

    Update busca.js

--- scripts/busca.js
+++ scripts/busca.js
@@ -191,8 +191,8 @@ function recibir() {
                         </h2>
                         <h2 class="name" onclick="asisInfo();" >
                             Asistencias: <span class="tamaño ">${newArray[0].TOTALASIS}</span>
-<div class="meter2 azul">
-			                <span style="width: ${(parseInt(newArray[0].TOTALASIS)*100)/12}%"></span>
+<div class="meter azul">
+			                <span class="animate" style="width: ${(parseInt(newArray[0].TOTALASIS)*100)/12}%"></span>
 		                </div>
 
                         </h2>
